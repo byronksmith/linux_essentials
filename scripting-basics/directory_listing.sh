adb02c7537d8766320fa8314c57f9a5423815821
Byron Smith <50594153+byronksmith@users.noreply.github.com>
2022-11-30 17:08:52 -0600

    Create directory_listing.sh

---
+++ scripting-basics/directory_listing.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# A simple script to put the file in the current directory to the screen
+# Author: Byron Smith
+# Date: Nov 30, 2022
+
+echo "Your current directory is:"
+pwd
+
+echo "The files in your current directory are:"
+ls
